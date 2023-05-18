be3bae7e389724b19be0d91db2408fdcf7d918f1
ebarbosaf <eri.barbosarj@gmail.com>
2023-05-18 11:53:13 -0300

    configurando eureka server

--- ms-worker/ms-worker/src/main/java/com/msworker/repositories/WorkerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.msworker.repositories;
-
-import com.msworker.entities.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-}
--- ms-worker/ms-worker/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tb_worker (name, daily_in_come) VALUES ('Bob', 200.0);
-INSERT INTO tb_worker (name, daily_in_come) VALUES ('Maria', 300.0);
-INSERT INTO tb_worker (name, daily_in_come) VALUES ('Alex', 250.0);
